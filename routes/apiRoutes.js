f4a93ec21502f94561a510d613a35242b844775c
chelle <maphaiyarath@gmail.com>
2021-02-28 00:46:00 -0600

    added api and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const Transaction = require("../models/transaction.js");
+
+module.exports = function(app) {
+    app.post("/api/transaction", ({body}, res) => {
+        Transaction.create(body).then(dbTransaction => {
+            res.json(dbTransaction);
+        }).catch(err => {
+            res.status(404).json(err);
+        });
+    });
+      
+    app.post("/api/transaction/bulk", ({body}, res) => {
+        Transaction.insertMany(body).then(dbTransaction => {
+            res.json(dbTransaction);
+        }).catch(err => {
+            res.status(404).json(err);
+        });
+    });
+      
+    app.get("/api/transaction", (req, res) => {
+        Transaction.find({}).sort({date: -1}).then(dbTransaction => {
+            res.json(dbTransaction);
+        }).catch(err => {
+            res.status(404).json(err);
+        });
+    });
+};
